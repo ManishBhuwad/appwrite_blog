2a12b10e112d1f3a9cc19a5241201577d912cc53
ManishBhuwad <96858360+ManishBhuwad@users.noreply.github.com>
2023-11-06 11:33:15 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo(width = '100px') {
   return (
-    <div className=' flex-none sm:flex items-center text-red-600'><img src="src\assets\favicon-32x32.png" alt="" /><span>ManishBhuwad</span></div>
+    <div className=' flex-none sm:flex items-center text-red-600'><img src="src/assets/favicon-32x32.png" alt="icon" /><span>ManishBhuwad</span></div>
   )
 }
 
