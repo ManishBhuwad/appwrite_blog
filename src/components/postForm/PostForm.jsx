8625a95808c6c9cb4584299f4ac260009c90887b
ManishBhuwad <96858360+ManishBhuwad@users.noreply.github.com>
2023-12-13 14:29:45 +0530

    Update PostForm.jsx
    hover color change

--- src/components/postForm/PostForm.jsx
+++ src/components/postForm/PostForm.jsx
@@ -113,7 +113,7 @@ export default function PostForm({post}) {
                     className="mb-4"
                     {...register("status", { required: true })}
                 />
-                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full hover:bg-blue-800">
                     {post ? "Update" : "Submit"}
                 </Button>
             </div>
