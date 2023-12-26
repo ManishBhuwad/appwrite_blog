880d69fc4c4cd3e402ccdbe63ac253357ad5391b
ManishBhuwad <96858360+ManishBhuwad@users.noreply.github.com>
2023-12-26 15:50:38 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import myimage from '../assets/logo3.png'
+import myimage from '../assets/mdb-high-resolution-logo.png'
 
 function Logo(width = '100px') {
   return (
-    <div className=' flex-none sm:flex items-center text-red-600'><img src={myimage} alt="logo" className='w-12' /></div>
+    <div className=' flex-none sm:flex items-center text-red-600'><img src={myimage} alt="logo-img" className='w-12' /></div>
   )
 }
 
